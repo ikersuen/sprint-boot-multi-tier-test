9ab7643a1cf15db34a71c18ba233cb1b777f1747
Iker Suen <apple@appledeMacBook-Air-2.local>
2018-12-02 01:11:14 +0800

    [iker]chore: add ParkingBoy Controller POST mapping

--- src/main/java/com/oocl/web/sampleWebApp/controllers/ParkingBoyResource.java
+++ src/main/java/com/oocl/web/sampleWebApp/controllers/ParkingBoyResource.java
@@ -1,12 +1,13 @@
 package com.oocl.web.sampleWebApp.controllers;
 
+import com.oocl.web.sampleWebApp.domain.ParkingBoy;
 import com.oocl.web.sampleWebApp.domain.ParkingBoyRepository;
 import com.oocl.web.sampleWebApp.models.ParkingBoyResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/parkingboys")
@@ -22,4 +23,23 @@ public class ParkingBoyResource {
             .toArray(ParkingBoyResponse[]::new);
         return ResponseEntity.ok(parkingBoys);
     }
+
+    @PostMapping
+    public ResponseEntity<String> addNewParkingBoy(@RequestBody ParkingBoy parkingBoy){
+        if (parkingBoyRepository.save(parkingBoy) != null){
+            return buildCreateResponse(parkingBoy);
+        }
+            return buildCreateFailResponse();
+    }
+
+    private ResponseEntity<String> buildCreateResponse(ParkingBoy parkingBoy){
+        Long parkingBoyId = parkingBoy.getId();
+        URI location = URI.create("/parkingboys/" + parkingBoyId);
+        return ResponseEntity.created(location).build();
+    }
+
+    private ResponseEntity<String> buildCreateFailResponse(){
+        return ResponseEntity.badRequest().build();
+    }
+
 }
