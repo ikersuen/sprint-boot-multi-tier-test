15d05663a0a68fec65321ae9c7190f06c7aaef74
Iker Suen <apple@appledeMacBook-Air-2.local>
2018-12-02 02:14:44 +0800

    [iker] test: should create parking boy with non empty string

--- src/test/java/com/oocl/web/sampleWebApp/ParkingBoyTest.java
+++ src/test/java/com/oocl/web/sampleWebApp/ParkingBoyTest.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -17,12 +18,15 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static com.oocl.web.sampleWebApp.WebTestUtil.getContentAsObject;
+import static org.hamcrest.Matchers.containsString;
 import static org.junit.Assert.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
-public class SampleWebAppApplicationTests {
+public class ParkingBoyTest {
     @Autowired
     private ParkingBoyRepository parkingBoyRepository;
 
@@ -46,5 +50,21 @@ public class SampleWebAppApplicationTests {
 
         assertEquals(1, parkingBoys.length);
         assertEquals("boy", parkingBoys[0].getEmployeeId());
+    }
+
+    @Test
+    public void should_create_parking_boy_with_non_empty_string() throws Exception{
+	    //Given a parkingboy {"employeeID":"String"} which employeeID is a non-empty string and unique ID
+        int employeeId = 14120;
+        String createTestParkingBoyJson = "{\"employeeId\":" + employeeId + "}";
+
+        //When POST to /parkingboys
+        mvc.perform(post("/parkingboys")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(createTestParkingBoyJson)
+        )
+        //Then it should return 201 Created
+                .andExpect(status().isCreated())
+                .andExpect(header().string("Location", containsString("/parkingboys/" + employeeId)));
     }
 }
